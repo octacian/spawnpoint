8bff3bf0b19c4310054b150b19ae525fca481fe5
octacian <enduffy2014@outlook.com>
2017-03-08 12:34:22 -0800

    Add logger

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
 -- spawnpoint/init.lua
+
+spawnpoint = {}
+
+local path = minetest.get_worldpath().."/spawnpoint.conf"
+
+-- [function] Log
+function spawnpoint.log(content, log_type)
+  if not content then return false end
+  if log_type == nil then log_type = "action" end
+  minetest.log(log_type, "[HUD Plus] "..content)
+end
